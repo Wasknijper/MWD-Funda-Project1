3952e36a7c58c1291d871a4df9b2aba49e5f79b0
Wasknijper <mudkippers@hotmail.com>
2016-02-22 14:22:46 +0100

    geo testing

--- static/app.js
+++ static/app.js
@@ -3,10 +3,12 @@
 	var app = {
 		oldRoute : "",
 		newRoute : "",
-		apiUrl : "https://funda.kyrandia.nl/feeds/Aanbod.svc/json/e2d60e885b8742d4b0648300e3703bd7/",
+		fundaUrl : "https://funda.kyrandia.nl/feeds/Aanbod.svc/json/e2d60e885b8742d4b0648300e3703bd7/",
+		googleApi : "https://maps.googleapis.com/maps/api/geocode/json?",
+		googleKey : "&key=AIzaSyDyViN5tiPa3bD0qtBjZj8ejkFyp8UOXNY",
 		init: function(){
 			//stuff happening
-			var dd = utils.request('?type=koop&zo=/amsterdam/tuin/&pagesize=25');
+			var dd = utils.request(fundaUrl, '?type=koop&zo=/amsterdam/tuin/&pagesize=25');
 			dd.then(
 				function(d){
 					console.log(d);
@@ -14,7 +16,16 @@
 			);
 
 			navigator.geolocation.getCurrentPosition(function(position) {
- 				console.log(position.coords.latitude, position.coords.longitude);
+				var lat = position.coords.latitude;
+				var long = position.coords.longitude;
+				var latLong = 'latlng=' + lat + ',' + long;
+
+				var postcode = utils.request(googleApi, latLong, googleKey);
+				postcode.then(
+					function(d){
+						console.log(d);
+					}
+				)
 			});
 
 			utils.processHash();
@@ -29,9 +40,13 @@
 			app.newRoute = hash.slice(1);
 			router.run(app.newRoute);
 		},
-		request : function(options){
+		request : function(url, options, key){
 			//call pegasus libirary for the ajax get
-			return pegasus(app.apiUrl + options);
+			if(key){
+				return pegasus(url + options + key);	
+			} else {
+				return pegasus(url + options);
+			}
 		}
 	};
 
